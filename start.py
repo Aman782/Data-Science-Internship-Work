e44eab82d6f2408af6bdb149b9dbc1fd9787d001
Aman Pandey <amanpandey45692@gmail.com>
2024-07-02 11:56:02 +0530

    Secondday commit

--- start.py
+++ start.py
@@ -30,3 +30,16 @@ print(list)
 
 list.clear()
 print(list)
+
+
+
+w = {1, 7, 8, 8, 5, 6,3, 4, 1, 1, 9, 7}  #Unordered collecion of elements
+print(w)
+ 
+e = [5, 7, 8, 9, 6, 3, 4, 5, 6, 2, 3]
+print(e)
+
+print(e[3:7:1])
+
+print(e[0:7:2])
+
